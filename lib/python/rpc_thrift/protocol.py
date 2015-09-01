297cb14f8552e7b89fe998a55da1302bf7d3c9a5
feiwang <wangfei@chunyu.me>
2015-09-01 18:00:58 +0800

    修改变量

--- lib/python/rpc_thrift/protocol.py
+++ lib/python/rpc_thrift/protocol.py
@@ -4,12 +4,12 @@ from thrift.protocol.TBinaryProtocol import TBinaryProtocol
 
 
 class TUtf8BinaryProtocol(TBinaryProtocol):
-    def writeString(self, str):
+    def writeString(self, v):
         """
             只要控制好了writeString, 在整个thrift系统中，所有的字符串都是utf-8格式的
         """
-        if isinstance(str, unicode):
-            str = str.encode("utf-8")
+        if isinstance(v, unicode):
+            v = v.encode("utf-8")
 
         # TBinaryProtocol 为 old style class
-        TBinaryProtocol.writeString(self, str)
+        TBinaryProtocol.writeString(self, v)
