d6c2dcf271e7bea79ddbfe93d9699863b3b33eff
feiwang <wangfei@chunyu.me>
2015-09-06 17:17:00 +0800

    改进了proxy的部署脚本

--- scripts/deploy_proxy_online.sh
+++ scripts/deploy_proxy_online.sh
@@ -23,7 +23,7 @@ echo "ssh root@${host_name} rm -f /usr/local/rpc_proxy/bin/rpc_proxy"
 ssh root@${host_name} "rm -f /usr/local/rpc_proxy/bin/rpc_proxy"
 
 echo "scp rpc_lb root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy"
-scp rpc_lb root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy
+scp rpc_proxy root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy
 
 # 拷贝脚本
 scp git.chunyu.me/infra/rpc_proxy/scripts/control_lb.sh    root@${host_name}:/usr/local/rpc_proxy/
--- scripts/deploy_proxy_test.sh
+++ scripts/deploy_proxy_test.sh
@@ -22,8 +22,8 @@ scp rpc_lb root@${host_name}:/usr/local/rpc_proxy/bin/rpc_lb
 echo "ssh root@${host_name} rm -f /usr/local/rpc_proxy/bin/rpc_proxy"
 ssh root@${host_name} "rm -f /usr/local/rpc_proxy/bin/rpc_proxy"
 
-echo "scp rpc_lb root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy"
-scp rpc_lb root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy
+echo "scp rpc_proxy root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy"
+scp rpc_proxy root@${host_name}:/usr/local/rpc_proxy/bin/rpc_proxy
 
 # 拷贝脚本
 scp git.chunyu.me/infra/rpc_proxy/scripts/control_lb.sh    root@${host_name}:/usr/local/rpc_proxy/
