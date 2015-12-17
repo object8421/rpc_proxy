745fdc4474b6678e805cc130fa45f7fc74a8a43f
feiwang <wangfei@chunyu.me>
2015-12-17 12:22:20 +0800

    增加了请求过期的时间

--- proxy/consts.go
+++ proxy/consts.go
@@ -6,7 +6,7 @@ const (
 	BACKEND_CONN_MAX_SEQ_ID    = 1000000     // 100万次请求一个轮回(也挺不容易的)
 	INVALID_ARRAY_INDEX        = -1          // 无效的数组元素下标
 	HB_TIMEOUT                 = 6           // 心跳超时时间间隔
-	REQUEST_EXPIRED_TIME_MICRO = 25 * 100000 // 2.5s
+	REQUEST_EXPIRED_TIME_MICRO = 5 * 1000000 // 5s
 	TEST_PRODUCT_NAME          = "test"
-	VERSION                    = "0.1.0-2015090621" // 版本信息
+	VERSION                    = "0.1.0-2015121712" // 版本信息
 )
